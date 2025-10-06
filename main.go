552848737ca0dc840be4230558ed0f296665fda3
Persicaria_QLozin <ManInBilibili@qq.com>
2025-10-06 17:25:52 +0800

    update

--- main.go
+++ main.go
@@ -11,52 +11,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	"sync"
 	"time"
-)
-
-// DNSHeader 表示DNS消息头
-type DNSHeader struct {
-	ID      uint16
-	Flags   uint16
-	QDCount uint16
-	ANCount uint16
-	NSCount uint16
-	ARCount uint16
-}
-
-// DNSQuestion 表示DNS查询部分
-type DNSQuestion struct {
-	Name   string
-	QType  uint16
-	QClass uint16
-}
-
-// DNSRecord 表示DNS记录
-type DNSRecord struct {
-	Name     string
-	Type     uint16
-	Class    uint16
-	TTL      uint32
-	RDLength uint16
-	rdata    []byte
-}
-
-// DNSLogEntry 表示DNS查询日志条目
-type DNSLogEntry struct {
-	Timestamp     time.Time `json:"timestamp"`
-	ClientIP      string    `json:"client_ip"`
-	TransactionID uint16    `json:"transaction_id"`
-	DomainName    string    `json:"domain_name"`
-	QueryType     uint16    `json:"query_type"`
-	QueryClass    uint16    `json:"query_class"`
-	Result        string    `json:"result"`
-}
-
-var (
-	logFile     *os.File
-	logMutex    sync.Mutex
-	upstreamDNS = "100.90.80.129:5353"
 )
 
 // 解析DNS消息
@@ -617,11 +572,10 @@ func handleUDPQuery(conn *net.UDPConn, addr *net.UDPAddr, data []byte) {
 			log.Printf("Failed to add EDNS Client Subnet: %v", ecdnsErr)
 			// 继续使用原始数据
 			forwardData = data
-		} else {
-			// 验证EDNS Client Subnet扩展是否被正确添加
-			hasECS := hasEDNSClientSubnet(forwardData)
-			log.Printf("EDNS Client Subnet added for %s, ECS extension present: %v", addr.IP.String(), hasECS)
 		}
+		// 验证EDNS Client Subnet扩展是否被正确添加
+		// hasECS := hasEDNSClientSubnet(forwardData)
+		// log.Printf("EDNS Client Subnet added for %s, ECS extension present: %v", addr.IP.String(), hasECS)
 	}
 
 	// 将请求转发到上游DNS服务器
